e4147df0390508339594f179479dd04c49a56431
ddguerra90 <ddguerran@correo.udistrital.edu.co>
2021-02-19 11:25:31 -0500

    fix: correccion variables de entorno

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -19,6 +19,7 @@ export const environment = {
   EVALUACIONMID_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_mid/v1/',
   EVALUACIONCRUD_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/evaluacion_crud/v1/',
   ADMINISTRIVA_AMAZON: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/administrativa_amazon_api/v1/',
+  DOCUMENTO_SERVICE: 'https://autenticacion.portaloas.udistrital.edu.co/apioas/documento_crud/v2/',
 
   SPAGOBI: {
     PROTOCOL: 'https',
