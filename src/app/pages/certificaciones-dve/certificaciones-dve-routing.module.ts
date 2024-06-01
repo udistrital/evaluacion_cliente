70da72b693101c873cd3051df6dea7ce9020822f
stevencol <steven2088@hotmail.es>
2024-05-31 22:32:59 -0500

    feat: Creacion de certificaciones.dve y  implemtacion del formulario-certificaciones-dev udistrital/Cumplidos_y_certificaciones_documentacion#34

---
+++ src/app/pages/certificaciones-dve/certificaciones-dve-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CertificacionesDveComponent } from "./certificaciones-dve.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: CertificacionesDveComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CertificacionesDveRoutingModule {}
